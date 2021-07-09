100e60e46dff335ef70cea2917820b36d4603714
Luke McElnea <luke.mcelnea@gmail.com>
2021-07-09 23:48:57 +1000

    added ffmpeg static prebuilt libs as modules

--- TelloVideoDecoder/TelloVideoDecoder/Android.mk
+++ TelloVideoDecoder/TelloVideoDecoder/Android.mk
@@ -1,3 +1,64 @@
+# ------------------------------------ libavutil
+include $(CLEAR_VARS)
+
+LOCAL_PATH      := $(NDK_PROJECT_PATH)
+LOCAL_MODULE    := avutil
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/ffmpeg-android-prebuilt/include
+LOCAL_SRC_FILES := $(LOCAL_PATH)/ffmpeg-android-prebuilt/lib/libavutil.a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+# ------------------------------------ libavformat
+include $(CLEAR_VARS)
+
+LOCAL_PATH      := $(NDK_PROJECT_PATH)
+LOCAL_MODULE    := avformat
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/ffmpeg-android-prebuilt/include
+LOCAL_SRC_FILES := $(LOCAL_PATH)/ffmpeg-android-prebuilt/lib/libavformat.a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+# ------------------------------------ libavcodec
+include $(CLEAR_VARS)
+
+LOCAL_PATH      := $(NDK_PROJECT_PATH)
+LOCAL_MODULE    := avcodec
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/ffmpeg-android-prebuilt/include
+LOCAL_SRC_FILES := $(LOCAL_PATH)/ffmpeg-android-prebuilt/lib/libavcodec.a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+# ------------------------------------ libswscale
+include $(CLEAR_VARS)
+
+LOCAL_PATH      := $(NDK_PROJECT_PATH)
+LOCAL_MODULE    := swscale
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/ffmpeg-android-prebuilt/include
+LOCAL_SRC_FILES := $(LOCAL_PATH)/ffmpeg-android-prebuilt/lib/libswscale.a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+# ------------------------------------ libswresample
+include $(CLEAR_VARS)
+
+LOCAL_PATH      := $(NDK_PROJECT_PATH)
+LOCAL_MODULE    := swresample
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/ffmpeg-android-prebuilt/include
+LOCAL_SRC_FILES := $(LOCAL_PATH)/ffmpeg-android-prebuilt/lib/libswresample.a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+# ------------------------------------ libx264
+include $(CLEAR_VARS)
+
+LOCAL_PATH      := $(NDK_PROJECT_PATH)
+LOCAL_MODULE    := libx264
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/ffmpeg-android-prebuilt/include
+LOCAL_SRC_FILES := $(LOCAL_PATH)/ffmpeg-android-prebuilt/lib/libx264.a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+# ------------------------------------ libTelloVideoDecoder
 include $(CLEAR_VARS)
 
 # override strip command to strip all symbols from output library; no need to ship with those..
@@ -12,19 +73,21 @@ FFMPEG_LIB_PATH := $(LOCAL_PATH)/ffmpeg-android-prebuilt/lib
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/ffmpeg-android-prebuilt/include
 LOCAL_SRC_FILES := RenderAPI.cpp RenderAPI_OpenGLCoreES.cpp RenderingPlugin.cpp TelloVideoDecoder.cpp
 
-LOCAL_LDFLAGS 	:= -L$(FFMPEG_LIB_PATH)
+#LOCAL_LDFLAGS 	:= -v
+LOCAL_LDFLAGS 	:= -L$(FFMPEG_LIB_PATH) -v
 
-LOCAL_LDLIBS    := -Wl,--no-warn-shared-textrel \
+LOCAL_LDLIBS    := \
 	-llog -lEGL -lGLESv1_CM -lGLESv2 -lz \
-	-lavformat -lavcodec -lswresample -lswscale -lavutil -lx264
+#	-lavformat -lavcodec -lswresample -lswscale -lavutil -lx264
+# -Wl,--no-warn-shared-textrel
 
-LOCAL_STATIC_LIBRARIES := avcodec avformat avutil swscale x264
+LOCAL_STATIC_LIBRARIES := avutil avcodec avformat swresample swscale x264
 
-APP_ALLOW_MISSING_DEPS=true
+APP_ALLOW_MISSING_DEPS=false
 
 ifeq ($(NDK_DEBUG), 0)
 LOCAL_CFLAGS += -DNDEBUG
 endif
 
 include $(BUILD_SHARED_LIBRARY)
-#$(call import-add-path, $(FFMPEG_LIB_PATH))
+# $(call import-add-path, $(FFMPEG_LIB_PATH) )
